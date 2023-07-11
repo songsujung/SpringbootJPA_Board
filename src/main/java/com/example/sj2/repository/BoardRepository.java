a3118025d5fa045c4c09b4ab635b3d52a4e59d13
sj <sujung033131@gmail.com>
2023-07-12 01:44:47 +0900

    0711 JPA BoardTest(Read, Update,Search_JPQL)

--- src/main/java/com/example/sj2/repository/BoardRepository.java
+++ src/main/java/com/example/sj2/repository/BoardRepository.java
@@ -1,9 +1,39 @@
 package com.example.sj2.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.sj2.domain.Board;
 
 public interface BoardRepository  extends JpaRepository<Board, Long> {
+
+    // 검색(title로 조회)
+    // title을 찾고, 결과 값을 Board타입의 리스트로 반환 - 방식1
+    List<Board> findByTitleContaining(String title);
+
+    // 검색(content로 전체조회)
+    // content를 찾고, 결과 값을 Board타입의 페이지로 반환(Pageable을 사용하면 Page로 반환해야함)
+    // 페이징처리까지 더해 content를 검색 - 방식2
+    Page<Board> findByContentContaining(String content, Pageable pageable);
+
+    // 검색(JPQL사용해서 title로 전체조회)
+    // : JPQL? 어노테이션을 사용해서 조건을 달아 값을 찾는 방식(항상 Param을 넣어 줘야됨)
+    // board를 b라는 별칭을 지정, b에 있는 title(b.title)과 파라미터값을 비교해서 완전히 같은 값만 조회
+    // @Query("select b from Board b where b.title = :title ") // = :파라미터값 <- 이형식 지켜야함
+    @Query("select b from Board b where b.title like %:title%") // like는 포함되기만 하면 조회됨
+    List<Board> searchTitle(@Param("title") String title);
+    
+    // 검색(JPQL사용, b.title로 비교해서 bno, title을 조회)
+    @Query("select b.bno,b.title from Board b where b.title like %:title%")
+    List<Object[]> searchTitle2(@Param("title") String title);
+
+    // 검색(JPQL사용, b.title로 비교해서 bno, title을 조회 + 페이징추가)
+    @Query("select b.bno,b.title from Board b where b.title like %:title%")
+    Page<Object[]> searchTitle3(@Param("title") String title, Pageable pageable);
     
 }
--- src/test/java/com/example/sj2/repository/BoardRepositoryTest.java
+++ src/test/java/com/example/sj2/repository/BoardRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.example.sj2.repository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.example.sj2.domain.Board;
 
@@ -17,16 +23,19 @@ public class BoardRepositoryTest {
     @Autowired
     private BoardRepository boardRepository;
 
+    // 100개의 데이터 생성
     @Test
     public void testInsert(){
 
-        Board board = Board.builder()
-        .title("title")
-        .content("content")
-        .writer("sujung")
-        .build();
+        for(int i = 0; i < 100; i++){
+            Board board = Board.builder()
+            .title("title" + i)
+            .content("content" + i)
+            .writer("sujung"+ (i%10))
+            .build();
 
-        boardRepository.save(board);
+            boardRepository.save(board);
+        }
 
     }
 
@@ -45,6 +54,90 @@ public class BoardRepositoryTest {
 
         log.info(result.get());
 
-        
     }
+
+    // 수정 (조회 후, 수정이 이뤄짐)
+    @Test
+    public void testUpdate() {
+
+        // 조회 후
+        long bno = 2L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+
+        // 조회된 값 수정
+        board.changeTitle("Update Title");
+
+        boardRepository.save(board);
+
+    }
+
+    // 검색(title로 조회)
+    @Test
+    public void testSearchTitle(){
+
+        // title에 숫자 1이 있으면 다 조회, 그 결과값을 Board타입의 리스트로 반환
+        List<Board> list = boardRepository.findByTitleContaining("1");
+
+        log.info(list);
+
+    }
+
+    // 검색(content로 전체조회 + 페이징처리)
+    @Test
+    public void testSearchContent(){
+
+        // 첫 번째 페이지(0번째)를 요청하고, 페이지 당 10개의 결과를 반환하도록 페이징 처리를 설정하며, bno를 내림차순으로 정렬
+        Pageable pageable = PageRequest.of(0,10,Sort.by("bno").descending());
+
+        // content에 1이 들어간 값을 찾고, pageable로 페이징 처리
+        Page<Board> result = boardRepository.findByContentContaining("1", pageable);
+
+        log.info("-----------------------");
+        log.info(result);
+
+    }
+
+    // 검색(JPQL사용해서 title로 전체조회)
+    @Test
+    public void testSearchTitle_jpql(){
+
+        List<Board> list = boardRepository.searchTitle("1");
+
+        log.info("-----------------");
+        log.info(list.size());
+        log.info(list);
+    }
+
+    // 검색(JPQL사용, b.title로 비교해서 bno, title을 조회)
+    @Test
+    public void testSearchTitle2_jpql(){
+        
+        List<Object[]> list = boardRepository.searchTitle2("1");
+
+        log.info("-----------------");
+        log.info(list.size());
+        
+        // 배열들의 값 확인 (forEach)
+        list.forEach(arr -> log.info(Arrays.toString(arr)));
+
+    }
+
+    // 검색(JPQL사용, b.title로 비교해서 bno, title을 조회 + 페이징추가)
+    @Test
+    public void testSearchTitle3_jpql(){
+        
+        Pageable pageable = PageRequest.of(0, 10,  Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.searchTitle3("1", pageable);
+
+        log.info(result);
+
+    }
+    
+
+
+
 }
